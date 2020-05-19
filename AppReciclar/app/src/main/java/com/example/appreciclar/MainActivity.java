499587f381f8c910bc80c673b75cb1d733c4c6ac
sebastianmorera07 <jsmorera45@ucatolica.edu.co>
2020-05-19 02:24:03 -0500

    subida final
    TALLER 02 3 INTERFACES

---
+++ AppReciclar/app/src/main/java/com/example/appreciclar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.appreciclar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.appreciclar.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                finish();
+            }
+        });
+        button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
